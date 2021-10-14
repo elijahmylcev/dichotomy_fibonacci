f05ca177320144da8c1e921eee97ac25d60fea09
Elijah <elijahmylcev@gmail.com>
2021-10-14 22:51:34 +0300

    init --> added jest --> wrote specification

---
+++ dichotomy.test.js
@@ -0,0 +1,10 @@
+const fibonacci = require('./dichotomy');
+
+describe('Fibonacci sequence', () => {
+    test.todo('getFib() method must return an array')
+    test.todo('Each subsequent number in the sequence[array] is equal to the sum of the previous two numbers')
+});
+
+describe('getElement()', () => {
+    test.todo('Returns the index of the specified element in a sequence')
+});
