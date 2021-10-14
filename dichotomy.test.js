e6a680f96ddf5df3951d355b93a6d79e18f303c1
Elijah <elijahmylcev@gmail.com>
2021-10-14 23:05:20 +0300

    refact specification && wrote tests

--- dichotomy.test.js
+++ dichotomy.test.js
@@ -1,10 +1,24 @@
-const fibonacci = require('./dichotomy');
+const Fibonacci = require('./dichotomy');
 
 describe('Fibonacci sequence', () => {
-    test.todo('getFib() method must return an array')
-    test.todo('Each subsequent number in the sequence[array] is equal to the sum of the previous two numbers')
+  let fib
+  beforeEach(() => {
+    fib = new Fibonacci(10)
+  });
+  test('getFib() method must return an array', () => {
+    expect(Array.isArray(fib.getFib(5))).toBeTruthy();
+  });
+  test('Each subsequent number in the sequence[array] is equal to the sum of the previous two numbers', () => {
+    const expected = [0, 1, 1, 2, 3, 5, 8]
+    expect(fib).toEqual(expect.arrayContaining(expected));
+  });
 });
 
-describe('getElement()', () => {
-    test.todo('Returns the index of the specified element in a sequence')
+describe('getIndex()', () => {
+  test('Returns the index of the specified element in a sequence | null', () => {
+    const fibTest = new Fibonacci(9)
+    expect(fibTest.getElement(2)).toEqual(3);
+    expect(fibTest.getElement(13)).toEqual(7);
+    expect(fibTest.getElement(6)).toBeNull();
+  })
 });
