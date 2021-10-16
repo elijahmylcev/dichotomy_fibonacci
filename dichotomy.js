646508e08f82b589d99c0a606f7f2575632b6294
Elijah <elijahmylcev@gmail.com>
2021-10-16 23:01:54 +0300

    Refactoring code. Class constructor returns an array of numbers fibonacci

--- dichotomy.js
+++ dichotomy.js
@@ -1,14 +1,15 @@
 class Fibonacci {
   constructor(index) {
     this.index = index
-  }
-
-  getFib(index) {
     const fib = [0,1]
-    for (let i = 0; i < index; i++) {
+    for (let i = 0; i < this.index; i++) {
       fib.push(fib[i] + fib[i+1])
     }
     return fib
+  }
+
+  getElement(index) {
+
   }
 }
 
--- dichotomy.test.js
+++ dichotomy.test.js
@@ -3,18 +3,18 @@ const Fibonacci = require('./dichotomy');
 describe('Fibonacci sequence', () => {
   let fib
   beforeEach(() => {
-    fib = new Fibonacci()
+    fib = new Fibonacci(10)
   });
-  test('getFib() method must return an array', () => {
-    expect(Array.isArray(fib.getFib(5))).toBeTruthy();
+  test('Fibonacci return an array', () => {
+    expect(Array.isArray(fib)).toBeTruthy();
   });
   test('Each subsequent number in the sequence[array] is equal to the sum of the previous two numbers', () => {
     const expected = [0, 1, 1, 2, 3, 5, 8]
-    expect(fib.getFib(10)).toEqual(expect.arrayContaining(expected));
+    expect(fib).toEqual(expect.arrayContaining(expected));
   });
 });
 
-describe('getIndex()', () => {
+describe('getElement()', () => {
   test('Returns the index of the specified element in a sequence | null', () => {
     const fibTest = new Fibonacci(9)
     expect(fibTest.getElement(2)).toEqual(3);
